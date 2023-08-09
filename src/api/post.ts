199726fe0d792d700cf7dcabc95da01f0384c946
steinjun0 <1142308@naver.com>
2023-08-09 17:14:13 +0900

    disable cache for getPosts

--- src/api/post.ts
+++ src/api/post.ts
@@ -2,7 +2,7 @@ import { TCompany } from "@/interface/post";
 import { IPost } from "@/scraping/scraping";
 
 export async function getPosts({ pageParam = { page: 1, companies: [] } }): Promise<{ posts: IPost[], isFinish: boolean; }> {
-  return fetch(`/api/post?page=` + pageParam.page + (pageParam.companies ? pageParam.companies.map((company: TCompany) => `&company=${company}`).join('') : ''))
+  return fetch(`/api/post?page=` + pageParam.page + (pageParam.companies ? pageParam.companies.map((company: TCompany) => `&company=${company}`).join('') : ''), { cache: "no-cache" })
     .then((res) => res.json())
     .then((res) => {
       const temp = res;
