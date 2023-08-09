1a2116bb3d3662f54aa1d1832f3cc38997adf1fe
steinjun0 <1142308@naver.com>
2023-08-09 17:09:32 +0900

    add nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,17 @@ import { Posts } from './posts';
 
 export default async function Home() {
 
-  const initialData = await getPosts({ page: 1 });
+  const initialData = await getPosts({ page: 0 });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12">
-      <Posts initialData={{ page: 1, ...initialData }} />
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 pt-0">
+      <nav className='fixed flex justify-between h-20 items-center w-full p-4 bg-white border-b'>
+        <h1 className='text-2xl font-semibold'>Tech Blog Hub</h1>
+        {/* <Image src='/logo/stein.svg' width='24' height='24' alt='stein' /> */}
+      </nav>
+      <div className='mt-24'>
+        <Posts initialData={{ page: 1, ...initialData }} />
+      </div>
     </main>
-
   );
 }
