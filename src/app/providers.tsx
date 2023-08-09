25768ab385cd3f25b56215eefe38689aee9ee034
steinjun0 <1142308@naver.com>
2023-08-09 17:27:06 +0900

    install react-query-devtools

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { PropsWithChildren, useState } from 'react';
 
 export default function Providers(props: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{props.children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{props.children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
