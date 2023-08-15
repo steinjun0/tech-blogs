c6c1026b7d8a01b2210c3bf78b05096a37aaeca4
steinjun0 <1142308@naver.com>
2023-08-15 15:48:55 +0900

    add .env.local

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -19,10 +19,11 @@ export async function GET(request: Request) {
 async function getConnection() {
   let mysql = require('mysql2/promise');
   const connection = await mysql.createConnection({
-    host: 'mysql',
-    user: 'jun',
-    password: '2134jkl!@#$1234jkl',
-    database: 'tech-blog-mysql'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   });
   return connection;
 }
